4cdd76ce3129d0617e24dd284384cd5f2212ad27
mohamed <med.mhiri.5@gmail.com>
2019-12-09 00:00:06 +0100

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import store from './store/index.js';
 import Count from './components/count.js';
 import List from './components/list.js';
 import Status from './components/status.js';
+import '../css/global.css';
 
 const formElement = document.querySelector('.js-form');
 const inputElement = document.querySelector('#new-item-field');
